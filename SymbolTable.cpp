e9ed9737ce277462f76ff9d5d835ff93b761a922
josiahgodfrey <rusanyujosiah7@gmail.com>
2025-01-05 15:43:44 +0300

    ten commit

--- SymbolTable.cpp
+++ SymbolTable.cpp
@@ -1,9 +1,43 @@
 #include "SymbolTable.h"
 
 void SymbolTable::setVariable(std::string name, int value) {
-    // Implement the logic to set the value of a variable in the symbol table
+    // Check if the variable already exists in the symbol table
+    if (variables.find(name) != variables.end()) {
+        std::cerr << "Warning: Variable " << name << " already exists in the symbol table. Overwriting value." << std::endl;
+    }
+
+    // Set the value of the variable in the symbol table
+    variables[name] = value;
 }
 
 int SymbolTable::getVariable(std::string name) {
-    // Implement the logic to get the value of a variable from the symbol table
+    // Check if the variable exists in the symbol table
+    if (variables.find(name) == variables.end()) {
+        std::cerr << "Error: Variable " << name << " not found in the symbol table." << std::endl;
+        exit(1);
+    }
+
+    // Return the value of the variable from the symbol table
+    return variables[name];
+}
+
+void SymbolTable::addFunction(Function function) {
+    // Check if the function already exists in the symbol table
+    if (functions.find(function.name) != functions.end()) {
+        std::cerr << "Warning: Function " << function.name << " already exists in the symbol table. Overwriting function." << std::endl;
+    }
+
+    // Add the function to the symbol table
+    functions[function.name] = function;
+}
+
+Function SymbolTable::getFunction(std::string name) {
+    // Check if the function exists in the symbol table
+    if (functions.find(name) == functions.end()) {
+        std::cerr << "Error: Function " << name << " not found in the symbol table." << std::endl;
+        exit(1);
+    }
+
+    // Return the function from the symbol table
+    return functions[name];
 }
--- SymbolTable.h
+++ SymbolTable.h
@@ -3,31 +3,37 @@
 
 #include <iostream>
 #include <unordered_map>
+#include <vector>
 
+// Define the Function class
+class Function {
+public:
+    std::string name;
+    std::vector<std::string> parameters;
+    std::vector<Token> body;
+
+    Function(std::string n, std::vector<std::string> p, std::vector<Token> b)
+        : name(n), parameters(p), body(b) {}
+};
 class SymbolTable {
 private:
     std::unordered_map<std::string, int> variables;
     std::unordered_map<std::string, Function> functions;
 
 public:
-    void setVariable(std::string name, int value);
-    int getVariable(std::string name);
-
-    void addVvariable(std::string name, int value) {
+    void setVariable(std::string name, int value) {
         variables[name] = value;
     }
 
-    int getVvariable(std::string name) {
-        if (variables.find(name) == variables.end()) {
+    int getVariable(std::string name) {
+        if (variables.find(name) != variables.end()) {
             return variables[name];
-        }
-        else {
+        } else {
             std::cerr << "Error: Variable " << name << " not found in symbol table." << std::endl;
             exit(1);
         }
     }
 
-//function handling logic
     void addFunction(Function function) {
         functions[function.name] = function;
     }
@@ -35,12 +41,31 @@ public:
     Function getFunction(std::string name) {
         if (functions.find(name) != functions.end()) {
             return functions[name];
-        }
-        else {
-            std::cerr << "Function not found: " << name << std::endl;
+        } else {
+            std::cerr << "Error: Function " << name << " not found in symbol table." << std::endl;
             exit(1);
         }
     }
+
+    // Add support for nested scopes
+    class Scope {
+    private:
+        std::unordered_map<std::string, int> variables;
+
+    public:
+        void setVariable(std::string name, int value) {
+            variables[name] = value;
+        }
+
+        int getVariable(std::string name) {
+            if (variables.find(name) != variables.end()) {
+                return variables[name];
+            } else {
+                std::cerr << "Error: Variable " << name << " not found in scope." << std::endl;
+                exit(1);
+            }
+        }
+    };
 };
 
 #endif
